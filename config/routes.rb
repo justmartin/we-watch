a0f1350d7932bc53009a2b718b0c4e008d5015b0
justmartin <justin.lamar.martin@gmail.com>
2016-11-03 12:28:50 -0700

    Removed leftover conflict in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-  devise_for :users
-  
-=======
+
   devise_for :users, controllers: { registrations: 'registrations' }
->>>>>>> master
+
   get "/" => "home#index"
 
   get "/movie" => "movie#info"
