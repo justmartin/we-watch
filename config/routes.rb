fe1502924a2c38e08ad2e121b3b9cb04b0ea962b
Cyntera <ccamper365@gmail.com>
2016-11-04 16:30:42 -0700

    Make home#index root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  get "/home" => "home#index"
+  root "home#index"
 
   get "/search" => "search#action"
 
