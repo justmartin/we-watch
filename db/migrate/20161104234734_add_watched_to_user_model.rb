662e30752adfa31df06305af039f724f1f42c396
justmartin <justin.lamar.martin@gmail.com>
2016-11-07 11:57:48 -0800

    Remove erroring line in migration

--- db/migrate/20161104234734_add_watched_to_user_model.rb
+++ db/migrate/20161104234734_add_watched_to_user_model.rb
@@ -1,8 +1,5 @@
 class AddWatchedToUserModel < ActiveRecord::Migration
   def change
-    
   	add_column :users, :watched_movie_ids, :text
-  	remove_column :users, :favorites_movie_ids
-
   end
 end
